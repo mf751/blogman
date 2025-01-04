841c61d76618a875f593f0495835498a7f84ea89
mf751 <mf751f@gmail.com>
2025-01-04 17:38:39 +0300

    added sql connection

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,28 +2,44 @@ package main
 
 import (
 	"crypto/tls"
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	"github.com/jackc/pgx/v4/stdlib"
 )
 
 type application struct {
 	errLogger  *log.Logger
 	infoLogger *log.Logger
+	db         *sql.DB
 }
 
 func main() {
 	infoLogger := log.New(os.Stdout, "[info ]\t", log.Ldate|log.Ltime)
 	errLogger := log.New(os.Stdout, "[info ]\t", log.Ldate|log.Ltime)
+
+	address := flag.String("addr", ":4001", "HTTP network address")
+	dsn := flag.String(
+		"dsn",
+		"postgres://postgres:1319@localhost:5432/blogman",
+		"Postgresql data source name",
+	)
+	flag.Parse()
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		errLogger.Fatal(err)
+	}
+
 	app := application{
 		errLogger:  errLogger,
 		infoLogger: infoLogger,
+		db:         db,
 	}
-
-	address := flag.String("addr", ":4001", "HTTP network address")
-	flag.Parse()
 
 	tlsConfig := &tls.Config{
 		CurvePreferences: []tls.CurveID{tls.X25519, tls.CurveP256},
@@ -40,8 +56,20 @@ func main() {
 	}
 
 	infoLogger.Printf("Starting server on %v", *address)
-	err := server.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
+	err = server.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
 	if err != nil {
 		errLogger.Fatal(err)
 	}
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	sql.Register("postgres", stdlib.GetDefaultDriver())
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
