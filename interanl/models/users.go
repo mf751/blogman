c4d4e5cd381409e47995138df70c12d71ea24b80
mf751 <mf751f@gmail.com>
2025-01-04 23:49:41 +0300

    updated users model

--- interanl/models/users.go
+++ interanl/models/users.go
@@ -17,37 +17,34 @@ type User struct {
 	HashedPassword []byte
 }
 
-type userModel struct {
+type usersModel struct {
 	DB *sql.DB
 }
 
-func (model *userModel) Insert(user User) (uuid.UUID, error) {
+func (model *usersModel) Insert(user User) (User, error) {
 	sqlStatment := `INSERT INTO users (id ,name, email, hashed_password, created) VALUES(
   $1, $2, $3, $4, $5)
-  RETURNING id;
+  RETURNING id, created;
   )`
-	var id uuid.UUID
 	err := model.DB.QueryRow(sqlStatment,
 		uuid.New().String(),
 		user.Name,
 		user.Email,
 		user.HashedPassword,
 		time.Now().UTC(),
-	).Scan(&id)
+	).Scan(&user.ID, &user.Created)
 	if errors.Is(err, sql.ErrNoRows) {
-		return uuid.UUID{}, ErrNoRecord
+		return User{}, ErrNoRecord
 	}
-	return id, err
+	return user, err
 }
 
-func (model *userModel) Get(id uuid.UUID) (User, error) {
-	sqlStatment := `SELECT * FROM users WHERE id=$1;`
-	user := User{}
+func (model *usersModel) Get(id uuid.UUID) (User, error) {
+	sqlStatment := `SELECT name, email, created FROM users WHERE id=$1;`
+	user := User{ID: id}
 	err := model.DB.QueryRow(sqlStatment, id.String()).Scan(
-		&user.ID,
 		&user.Name,
 		&user.Email,
-		&user.HashedPassword,
 		&user.Created,
 	)
 	if errors.Is(err, sql.ErrNoRows) {
@@ -56,7 +53,7 @@ func (model *userModel) Get(id uuid.UUID) (User, error) {
 	return user, err
 }
 
-func (model *userModel) Authenticate(user User, password string) error {
+func (model *usersModel) Authenticate(user User, password string) error {
 	sqlStatment := `SELECT id,hashed_password FROM users WHERE email=$1`
 	err := model.DB.QueryRow(sqlStatment, user.Email).Scan(&user.ID, &user.HashedPassword)
 	if err != nil {
@@ -75,26 +72,33 @@ func (model *userModel) Authenticate(user User, password string) error {
 	return nil
 }
 
-func (model *userModel) ChangePassword(user User, newPassword string) error {
+func (model *usersModel) ChangePassword(user User, newPassword string) (User, error) {
 	sqlStatment1 := `SELECT hashed_password From users WHERE id=$1`
-	sqlStatment2 := `UPDATE users SET hashed_password=$1 WHERE id=$2`
+	sqlStatment2 := `UPDATE users SET hashed_password=$1 WHERE id=$2 RETURNING name, email, created ;`
 	var password string
 	err := model.DB.QueryRow(sqlStatment1, user.ID).Scan(&password)
 	if err != nil {
-		return err
+		return User{}, err
 	}
 
 	if err = bcrypt.CompareHashAndPassword(user.HashedPassword, []byte(password)); err != nil {
 		if errors.Is(err, bcrypt.ErrMismatchedHashAndPassword) {
-			return ErrWrongCredintials
+			return User{}, ErrWrongCredintials
 		}
-		return err
+		return User{}, err
 	}
 	newHashedPassword, err := bcrypt.GenerateFromPassword([]byte(newPassword), 12)
 	if err != nil {
-		return err
+		return User{}, err
 	}
-	var id uuid.UUID
-	err = model.DB.QueryRow(sqlStatment2, string(newHashedPassword), user.ID).Scan(&id)
-	return err
+	user.HashedPassword = newHashedPassword
+	err = model.DB.QueryRow(sqlStatment2, string(newHashedPassword), user.ID).Scan(
+		&user.Name,
+		&user.Email,
+		&user.Created,
+	)
+	if err != nil {
+		return User{}, err
+	}
+	return user, nil
 }
